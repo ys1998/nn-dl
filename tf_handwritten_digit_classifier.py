fd6f36ebb53904344ab87b0cfed7f7128adad534
Yash <yashshah2398@gmail.com>
2017-12-07 18:40:33 +0530

    Added variations; Finally working!

--- tf_handwritten_digit_classifier.py
+++ tf_handwritten_digit_classifier.py
@@ -2,5 +2,6 @@ import tf_DNN
 import mnist_loader as ml
 
 tr_d, va_d, te_d = ml.load_data_wrapper()
+# Custom structures are possible - just change the layer sizes/number
 net=tf_DNN.DNN([784,30,10])
 net.train(tr_d)
